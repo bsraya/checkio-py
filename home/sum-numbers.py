2439d7737c0dbb0a103f987ca4c83e0e19e7042c
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2020-08-04 16:58:57 +0800

    Sum Numbers done

--- home/sum-numbers.py
+++ home/sum-numbers.py
@@ -1,3 +1,11 @@
-sentence = 'who is 1st there'
+def sum_numbers(sentence: str) -> int:
+    total = sum(int(word) for word in sentence.split() if word.isdigit())
+    return total
 
-print(sentence.split())
+lambda_sum_numbers = lambda sentence: sum(int(word) for word in sentence.split() if word.isdigit())
+
+sentence = 'who is 1st there'
+sentencetwo = '5 plus 6 is 11'
+
+print(sum_numbers(sentencetwo))
+print(lambda_sum_numbers(sentencetwo))
